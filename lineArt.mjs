c1048397648d65156b24851b118618413930d0cb
Margherita Ravanello <margherita.ravanello@gmail.com>
2022-07-27 21:32:03 +0200

    command line art

---
+++ lineArt.mjs
@@ -0,0 +1,10 @@
+import figlet from "figlet";
+
+figlet("> LineArt <", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
