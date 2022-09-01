6f64dad8366da907c598d7ef65b01564af9ccfea
Margherita Ravanello <63791162+RavaMarghe@users.noreply.github.com>
2022-09-01 15:12:46 +0200

    Configure Multer to save files to disk

--- Ex-createServer/src/server.test.ts
+++ Ex-createServer/src/server.test.ts
@@ -233,3 +233,32 @@ describe("DELETE /animals/:id", () => {
         expect(response.text).toContain("Cannot DELETE /animals/asdf");
     });
 });
+
+describe("POST /animals/:id/photo ", () => {
+    test("Valid request with PNG file upload", async () => {
+        await request
+            .post("/animals/2/photo")
+            .attach("photo", "Ex-createServer/test-fixtures/photos/file.png")
+            .expect(201)
+            .expect("Access-Control-Allow-Origin", "http://localhost:8080");
+    });
+
+
+    test("Invalid animal ID", async () => {
+        const response = await request
+            .post("/animals/asdf/photo")
+            .expect(404)
+            .expect("Content-Type", /text\/html/);
+
+        expect(response.text).toContain("Cannot POST /animals/asdf/photo");
+    });
+
+    test("Invalid test with no file upload", async () => {
+        const response = await request
+            .post("/animals/2/photo")
+            .expect(400)
+            .expect("Content-Type", /text\/html/);
+
+        expect(response.text).toContain("No photo file uploaded.");
+    });
+});
--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     testEnvironment: "node",
     verbose: true,
     clearMocks: true,
-    setupFilesAfterEnv: ["./Ex-createServer/src/lib/prisma/client.mock.ts"]
+    setupFilesAfterEnv: [
+        "./Ex-createServer/src/lib/prisma/client.mock.ts",
+        "./Ex-createServer/src/lib/middleware/multer.mock.ts",
+    ],
 };
