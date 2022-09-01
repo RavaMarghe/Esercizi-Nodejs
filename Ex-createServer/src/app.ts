d6c1df2a2026d827e1849cb118b7551a63a76bbd
Margherita Ravanello <63791162+RavaMarghe@users.noreply.github.com>
2022-09-01 22:07:19 +0200

    Refactor planets routes out of main app script

--- Ex-createServer/src/app.ts
+++ Ex-createServer/src/app.ts
@@ -1,16 +1,10 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
-import prisma from "./lib/prisma/client";
-import {
-    validate,
-    validationErrorMiddleware,
-    animalSchema,
-    AnimalData,
-} from "./lib/validation";
-import { initMulterMiddleware } from "./lib/middleware/multer";
 
-const upload = initMulterMiddleware();
+import { validationErrorMiddleware } from "./lib/validation";
+
+import animalsRoutes from "./routes/animals"
 
 const corsOptions = {
     origin: "http://localhost:8080",
@@ -22,103 +16,7 @@ app.use(express.json());
 
 app.use(cors(corsOptions));
 
-app.get("/animals", async (request, response) => {
-    const animals = await prisma.animal.findMany();
-
-    response.json(animals);
-});
-
-app.get("/animals/:id(\\d+)", async (request, response, next) => {
-    const animalID = Number(request.params.id);
-
-    const animal = await prisma.animal.findUnique({
-        where: { id: animalID },
-    });
-
-    if (!animal) {
-        response.status(404);
-        return next(`Cannot GET /animals/${animalID}`);
-    }
-
-    response.json(animal);
-});
-
-app.post(
-    "/animals",
-    validate({ body: animalSchema }),
-    async (request, response) => {
-        const animalData: AnimalData = request.body;
-
-        const animal = await prisma.animal.create({
-            data: animalData,
-        });
-
-        response.status(201).json(animal);
-    }
-);
-
-app.put(
-    "/animals/:id(\\d+)",
-    validate({ body: animalSchema }),
-    async (request, response, next) => {
-        const animalId = Number(request.params.id);
-        const animalData: AnimalData = request.body;
-
-        try {
-            const animal = await prisma.animal.update({
-                where: { id: animalId },
-                data: animalData,
-            });
-
-            response.status(200).json(animal);
-        } catch (error) {
-            response.status(404);
-            next(`Cannot PUT /animals/${animalId}`);
-        }
-    }
-);
-
-app.delete("/animals/:id(\\d+)", async (request, response, next) => {
-    const animalId = Number(request.params.id);
-
-    try {
-        await prisma.animal.delete({
-            where: { id: animalId },
-        });
-
-        response.status(204).end();
-    } catch (error) {
-        response.status(404);
-        next(`Cannot DELETE /animals/${animalId}`);
-    }
-});
-
-app.post(
-    "/animals/:id(\\d+)/photo",
-    upload.single("photo"),
-    async (request, response, next) => {
-        if (!request.file) {
-            response.status(400);
-            return next("No photo file uploaded.");
-        }
-
-        const animalId = Number(request.params.id);
-        const photoFilename = request.file.filename;
-
-        try {
-            await prisma.animal.update({
-                where: { id: animalId },
-                data: { photoFilename },
-            });
-            response.status(201).json({ photoFilename });
-        } catch (error) {
-            response.status(404);
-            next(`Cannot POST /animals/${animalId}/photo`);
-        }
-    }
-);
-
-app.use("/animals/photos", express.static("uploads"));
+app.use("/animals", animalsRoutes)
 
 app.use(validationErrorMiddleware);
 
