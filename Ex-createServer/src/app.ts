de051198589a4b41c7b1c09ee8792862ceced829
Margherita Ravanello <63791162+RavaMarghe@users.noreply.github.com>
2022-09-02 00:46:03 +0200

    Add and configure GitHub authentication strategy

--- Ex-createServer/src/app.ts
+++ Ex-createServer/src/app.ts
@@ -3,10 +3,16 @@ import "express-async-errors";
 
 import { validationErrorMiddleware } from "./lib/middleware/validation";
 import { initCorsMiddleware } from "./lib/middleware/cors";
+import { initSessionMiddleware } from "./lib/middleware/session";
+import { passport } from "./lib/middleware/passport";
 
 import animalsRoutes from "./routes/animals"
 
 const app = express();
+
+app.use(initSessionMiddleware());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.json());
 
