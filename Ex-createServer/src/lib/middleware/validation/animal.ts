8c38fa17dcb4919e131ab49461ecfe4d29964e97
Margherita <63791162+RavaMarghe@users.noreply.github.com>
2022-09-10 11:10:30 +0200

    Fix the adapter

--- Ex-createServer/src/lib/middleware/validation/animal.ts
+++
@@ -1,9 +0,0 @@
-import { Static, Type } from "@sinclair/typebox";
-
-export const animalSchema = Type.Object({
-    breed: Type.String(),
-    weight: Type.Integer(),
-    name: Type.Optional(Type.String()),
-}, {additionalProperties: false});
-
-export type AnimalData = Static<typeof animalSchema>
--- prisma/migrations/20220830111214_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Animal" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "breed" TEXT NOT NULL,
-    "weight" INTEGER NOT NULL,
-    "name" TEXT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
